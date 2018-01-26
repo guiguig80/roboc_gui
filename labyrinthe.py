a55edb54c5b4e929ccff2b837509ba4ae2141fb4
guiguig80 <35841280+guiguig80@users.noreply.github.com>
2018-01-26 15:05:43 +0100

    Add files via upload

---
+++ labyrinthe.py
@@ -0,0 +1,12 @@
+# -*-coding:Latin-1 -*
+
+"""Ce module contient la classe Labyrinthe."""
+
+class Labyrinthe:
+
+    """Classe représentant un labyrinthe."""
+
+    def __init__(self, robot, obstacles):
+        self.robot = robot
+        self.grille = {}
+        # ...
